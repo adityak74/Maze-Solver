1003952cdeaf0413d0cde3cdddab8453c6dfb670
Prabhat <prabhatracherlabhilai@gmail.com>
2015-10-19 12:27:47 +0530

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -17,35 +17,35 @@ def mazedisplay(grid,ch,end):
 	zeroes=(occurrences(grid, 0))
 	ones=(occurrences(grid, 1))	
 	for x in zeroes:	
-		pygame.draw.rect(screen,white,(x[1]*20,x[0]*20,width,height), thickness)
+		pygame.draw.rect(screen,white,(x[1]*15,x[0]*15,width,height), thickness)
 	for y in ones:
-		pygame.draw.rect(screen,black,(y[1]*20,y[0]*20,width,height), thickness)
+		pygame.draw.rect(screen,black,(y[1]*15,y[0]*15,width,height), thickness)
 	if(ch==1 and end==-1):
 		twos=(occurrences(grid, 2))
 		for z in twos:			
-			pygame.draw.rect(screen,red,(z[1]*20,z[0]*20,width,height), thickness)	
+			pygame.draw.rect(screen,red,(z[1]*15,z[0]*15,width,height), thickness)	
 	elif(ch==2):
 		a,b=end%10,end/10
-		pygame.draw.rect(screen,red,(b*20,a*20,width,height), thickness)		
+		pygame.draw.rect(screen,red,(b*15,a*15,width,height), thickness)		
 	pygame.display.flip()
 	pygame.display.update()
-	time.sleep(3)
+	#time.sleep(1)
 def path(a,b):
-	pygame.draw.rect(screen,blue,(b*20,a*20,width,height), thickness)
+	pygame.draw.rect(screen,blue,(b*15,a*15,width,height), thickness)
 	pygame.display.update()
 	time.sleep(1)
-	pygame.draw.rect(screen,white,(b*20,a*20,width,height), thickness)
+	pygame.draw.rect(screen,white,(b*15,a*15,width,height), thickness)
 	pygame.display.update()
 	pygame.display.flip()
 	pygame.display.update()
 def dest(a,b):
-	pygame.draw.rect(screen,blue,(b*20,a*20,width,height), thickness)
+	pygame.draw.rect(screen,blue,(b*15,a*15,width,height), thickness)
 	pygame.display.update()
 	pygame.display.flip()
 	pygame.display.update()
 	time.sleep(10)
 def visit(a,b):
-	pygame.draw.rect(screen,pink,(b*20,a*20,width,height), thickness)
+	pygame.draw.rect(screen,pink,(b*15,a*15,width,height), thickness)
 	pygame.display.update()
 	pygame.display.flip()
 	pygame.display.update()
@@ -55,10 +55,10 @@ def bfspath(path):
 		a,b=int(path[point])/10,int(path[point])%10
 		c=str(a)+str(b)
 		play.playtile(c)
-		pygame.draw.rect(screen,blue,(b*20,a*20,width,height), thickness)
+		pygame.draw.rect(screen,blue,(b*15,a*15,width,height), thickness)
 		pygame.display.update()
 		time.sleep(1)
-		pygame.draw.rect(screen,white,(b*20,a*20,width,height), thickness)
+		pygame.draw.rect(screen,white,(b*15,a*15,width,height), thickness)
 		pygame.display.update()
 		pygame.display.flip()
 		pygame.display.update()
